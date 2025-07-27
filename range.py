b09ba1a0d1a60ff541e4735b7f552f92cf0b76c0
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-27 15:31:13 +0530

    multiplication

--- range.py
+++ range.py
@@ -1,6 +1,6 @@
-number = 3
+number =3
 
-for i in range(1, 11):
-    if i == 5:
+for i in range(1,11):
+    if i==5:
         continue
-    print(number, 'x', i, '=', number * i)
+    print(number, 'x',i,'=',number*i)
