575b1b290c5719c369d9368aa99f3dc1d7a803aa
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-27 15:49:00 +0530

    factorial pf number

--- factorial.py
+++ factorial.py
@@ -1,10 +1,8 @@
 number = 5
-factorial = 1
+factorial=1
 
-while number > 0:
-    # factorial = factorial * number
-    # number = number - 1
-    factorial *= number
-    number -= 1
+while number>0:
+    factorial=factorial*number
+    number=number-1
+print("factorial",factorial)
 
-print("Factorial: ", factorial)
