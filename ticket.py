268af943f37d3fee150f19faf35286bde3d2840b
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-27 12:21:05 +0530

    ticket price

--- ticket.py
+++ ticket.py
@@ -1,10 +1,9 @@
-age = 26
-day = "Wednesday"
+age =26
+day = "wednesday"
 
-price = 12 if age >= 18 else 8
+price =12 if age>=18 else 8
 
-if day == "Wednesday":
-    # price = price - 2
-    price -= 2
+if day=="wednesday":
+    price=price-2
 
-print("Ticket price for you is $",price)
+print("ticket price for you is ",price)    
