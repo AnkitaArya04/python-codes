2598d3e4b63a9d4ee5da26adc5aa97a36c8e3003
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-27 15:42:15 +0530

    charcaters

--- character.py
+++ character.py
@@ -1,7 +1,6 @@
-input_str = "teeteracdacd"
+input_str="teeter"
 
 for char in input_str:
-    print(char)
-    if input_str.count(char) == 1:
-        print("Char is: ", char)
+    if input_str.count(char)==1:
+        print("char is ",char)
         break
