751753295b53681e02f71f910c924f6ad2960325
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-29 23:00:09 +0530

    oops in py

--- oop.py
+++ oop.py
@@ -5,14 +5,21 @@ class Car:
     
     def full_name(self):
         return f"{self.brand} {self.model}"
+    
+class ElectricCar(Car):
+    def __init__(self, brand,model,battery_size):
+        super().__init__(brand,model)
+        self.batter_size=battery_size
 
+my_tesla=ElectricCar("Tesla","Model S","85kwh")
+print(my_tesla.model)
+print(my_tesla.full_name())
 
+# my_car = Car("Toyota","Corolla")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
 
-my_car = Car("Toyota","Corolla")
-print(my_car.brand)
-print(my_car.model)
-print(my_car.full_name())
-
-my_new_car = Car("Tata","Safari")
-print(my_new_car.brand)
-print(my_new_car.model)
+# my_new_car = Car("Tata","Safari")
+# print(my_new_car.brand)
+# print(my_new_car.model)
