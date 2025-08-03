8253e263a396f91b9ec8c6acd64c60ed6b0ba2d1
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-03 23:32:16 +0530

    youtube manager

--- error_handling/youtubr_manager.py
+++ error_handling/youtubr_manager.py
@@ -6,6 +6,10 @@ def load_data():
     except FileNotFoundError:
             return []
 
+def save_data_helper(videos):
+    with open('youtube.txt','w') as file:
+        json.dump(videos,file)
+
 def list_all_videos(videos):
     pass
 
