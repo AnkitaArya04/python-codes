e304c6bab57645c24b6bcdc5c6233a056926f0a5
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-04 19:15:30 +0530

    sqlite yotube manager databasw

--- database_sqlite/youtube_manager_db.py
+++ database_sqlite/youtube_manager_db.py
@@ -11,3 +11,14 @@ cursor.execute('''
             time TEXT NOT NULL   
             )
 ''')
+def main():
+    while True:
+        print("\n Youtube manager app with DB")
+        print("1. List videos")
+        print("2. Add videos")
+        print("3. Update Videos")
+        print("4. Delete Videos")
+        print("5. Exit app")
+
+if __name__ == "__main__":
+    main()
