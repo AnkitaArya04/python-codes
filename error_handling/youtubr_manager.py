9c87ae2f5eae5b4b91e05a447a475e226b7ffc14
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-04 14:32:03 +0530

    youtube manager

--- error_handling/youtubr_manager.py
+++ error_handling/youtubr_manager.py
@@ -25,11 +25,27 @@ def add_video(videos):
     time=input("Enter video time:")
     videos.append({'name':name,'time':time})
     save_data_helper(videos)
+
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index=int(input("Enter the video number to update"))
+    if 1 <= index <= len(videos):
+        name=input("Enter the vudeo name")
+        time=input("Enter the new video time")
+        videos[index-1]={'name':name,'time':time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index is selected")
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index=int(input("Enter the video number to be deleted"))
+
+    if 1<=index<=len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("invalid video index selected")
 
 def main():
     videos=load_data()
