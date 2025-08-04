e10cf0938e66a2bd1c58ed146e3e0df383ece63b
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-04 13:26:20 +0530

    youtube manager

--- error_handling/youtubr_manager.py
+++ error_handling/youtubr_manager.py
@@ -3,7 +3,7 @@ def load_data():
     try:
         with open('youtube.txt','r') as file:
             test=json.load(file)
-            print(type(test))
+            # print(type(test))
             return test
     except FileNotFoundError:
             return []
@@ -13,8 +13,8 @@ def save_data_helper(videos):
         json.dump(videos,file)
 
 def list_all_videos(videos):
-    for index,video in enumerate(videos, start=1):
-        print(f"{index}. ")
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. {video['name']}, Duration: {video['time']} ")
 
 def add_video(videos):
     name=input("Enter video name:")
@@ -37,7 +37,7 @@ def main():
         print("4. Delete a youtube video")
         print("5. Exit the app")
         choice=input("Enter your choice")
-        print(videos)
+        # print(videos)
 
         match choice:
             case '1':
