ccc2c13958e92200d3d1571d9f1cf63617c1366f
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-06 23:26:20 +0530

    Free Api in python

--- handling_api/freeapi_username.py
+++ handling_api/freeapi_username.py
@@ -1,24 +1,24 @@
-# import requests
+import requests
 
-# def fectch_random_user_freeapi():
-#     url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
-#     response = requests.get(url)
-#     data = response.json()
+def fectch_random_user_freeapi():
+    url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+    response = requests.get(url)
+    data = response.json()
 
-#     if data["success"] and "data" in data:
-#         user_data = data["data"]
-#         username = user_data["login"]["username"]
-#         country = user_data["location"]["country"]
-#         return username,country
-#     else:
-#         raise Exception("Failed to fetch user data")
+    if data["success"] and "data" in data:
+        user_data = data["data"]
+        username = user_data["login"]["username"]
+        country = user_data["location"]["country"]
+        return username,country
+    else:
+        raise Exception("Failed to fetch user data")
     
-# def main():
-#     try:
-#         username, country = fectch_random_user_freeapi()
-#         print(f"Username: {username} \nCountry:{country}")
-#     except Exception as e:
-#         print(str(e))
+def main():
+    try:
+        username, country = fectch_random_user_freeapi()
+        print(f"Username: {username} \nCountry:{country}")
+    except Exception as e:
+        print(str(e))
 
-# if __name__=="__main__":
-#     main()
+if __name__=="__main__":
+    main()
